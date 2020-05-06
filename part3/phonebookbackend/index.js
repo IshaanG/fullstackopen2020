d7de43de0108d92b989daf82546eb9b13303fd9c
Ishaan Gupta <ishaang12@gmail.com>
2020-05-06 14:21:03 +0530

    3.10

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 morgan.token("data", (request) => {
@@ -9,6 +10,7 @@ morgan.token("data", (request) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
+app.use(cors());
 
 let persons = [
   {
@@ -87,7 +89,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
