dbfcfeeeab0f8df8b9a2c09394f76781d75261a1
Ishaan Gupta <ishaang12@gmail.com>
2020-05-11 20:21:25 +0530

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,25 @@ const totalLikes = (blogs) => {
   return blogs.reduce((a, b) => a + b.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+  let favoriteBlogObject = {
+    title: "a",
+    author: "b",
+    likes: -1,
+  };
+  blogs.forEach((blog) => {
+    if (blog.likes > favoriteBlogObject.likes) {
+      favoriteBlogObject.title = blog.title;
+      favoriteBlogObject.author = blog.author;
+      favoriteBlogObject.likes = blog.likes;
+    }
+  });
+  return favoriteBlogObject;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
